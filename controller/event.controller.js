25cc420592073545fae405ddc7c9beee639101f7
andela-ofamamkinde <olatoyosi.famakinde@andela.co>
2015-05-05 17:48:02 +0100

    Event controller created

--- controller/event.controller.js
+++ controller/event.controller.js
@@ -4,10 +4,31 @@ var Event = mongoose.model('events');
 exports.createevent = function(req, res){
   Event.create(req body, function(err, events){
     if (err){
-      res.send(err, events)
+      res.send(err)
     }
-    res.json();
+    res.json(events);
   });
-
 };
+
+exports.getevents = function(req, res){
+  Event.find(function(err, events){
+    if (err){
+      res.send(err)
+    }
+    res.json(events);
+  });
+};
+
+exports.deleteevents = function(req, res) {
+  Event.remove({
+    _id : req.params.user_id
+  },
+    function(err, events){
+    if(err) {
+      res.send(err);
+    }
+    res.json(events);
+  });
+};
+
 
--- routes.js
+++ routes.js
@@ -1,9 +1,6 @@
 module.exports =function(app){
   var controller = require('./controller/controller');
-
   app.post('/createnew', controller.creater);
-
   app.get('/viewuser', controller.finder);
-  
   app.delete('/deleteuser', controller.remover);
 }
