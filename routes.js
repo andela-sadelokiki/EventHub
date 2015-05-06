f41096ab6f4135c7c48d68a8ce706a3f2123382e
andela-ofamamkinde <olatoyosi.famakinde@andela.co>
2015-05-06 03:22:46 +0100

    CRUD operations completed in Event Controller

--- routes.js
+++ routes.js
@@ -1,9 +1,19 @@
 module.exports = function(app) {
 
   var userCtrl = require('./controller/user.controller');
+  var eventCtrl = require('./controller/event.controller');
 
   app.post('/signUp', userCtrl.createUser);
   app.get('/getAllUsers', userCtrl.getAllUser);
   app.delete('/deleteUser', userCtrl.deleteUser);
+
+
+  // EVENT ROUTES
+  app.post('/createEvent', eventCtrl.createEvent);
+  app.get('/getAllEvents', eventCtrl.getAllEvents);
+  
+  app.get('/getEvent/:event_id', eventCtrl.getOneEvent);
+  app.put('/editEvent/:event_id', eventCtrl.updateEvent);
+  app.delete('/deleteEvent/:event_id', eventCtrl.deleteEvent);
 
 }
